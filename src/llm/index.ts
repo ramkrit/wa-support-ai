3cb8e9383fb447e6e63dfc6c43ac061d48da3df0
ramkrit <rkrit851@gmail.com>
2026-06-06 23:30:18 +0530

    refactor: remove the unused files

--- src/llm/index.ts
+++
@@ -1,2 +0,0 @@
-export { LlmModule } from './llm.module';
-export { LlmService, ChatMessage, LlmResponse } from './llm.service';
--- src/wa-channel/index.ts
+++
@@ -1,2 +0,0 @@
-export { WaChannelModule } from './wa-channel.module';
-export { WaChannelService, WaClientStatus, WaStatusPayload } from './wa-channel.service';
